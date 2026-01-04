5c07cf65dfac397bfe23dd4b00a252da40db8de3
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-01-03 21:32:46 -0500

    Munging

--- Munging_dataset.py
+++ Munging_dataset.py
@@ -4,3 +4,12 @@ import pandas as pd
 import re
 
 from argparse import Namespace
+
+args = Namespace(
+    raw_dataset_csv="data/surnames/surnames.csv",
+    train_proportion=0.7,
+    val_proportion=0.15,
+    test_proportion=0.15,
+    output_munged_csv="data/surnames/surnames_with_splits.csv",
+    seed=1337
+)
