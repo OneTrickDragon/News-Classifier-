3e52ff5cc6c6b1341e79e4524e9a1c96d475ec87
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-01-13 00:52:55 -0500

    settings

--- RNN_classification.py
+++ RNN_classification.py
@@ -440,3 +440,38 @@ class SurnameClassifier(nn.Module):
                 y_out = F.softmax(y_out, dim=1)
 
             return y_out
+        
+
+def set_seed_everywhere(seed, cuda):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed_all(seed)
+
+def handle_dirs(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+
+
+args = Namespace(
+    # Data and path information
+    surname_csv="data/surnames/surnames_with_splits.csv",
+    vectorizer_file="vectorizer.json",
+    model_state_file="model.pth",
+    save_dir="model_storage/ch6/surname_classification",
+    # Model hyper parameter
+    char_embedding_size=100,
+    rnn_hidden_size=64,
+    # Training hyper parameter
+    num_epochs=100,
+    learning_rate=1e-3,
+    batch_size=64,
+    seed=1337,
+    early_stopping_criteria=5,
+    # Runtime hyper parameter
+    cuda=True,
+    catch_keyboard_interrupt=True,
+    reload_from_files=False,
+    expand_filepaths_to_save_dir=True,
+)
